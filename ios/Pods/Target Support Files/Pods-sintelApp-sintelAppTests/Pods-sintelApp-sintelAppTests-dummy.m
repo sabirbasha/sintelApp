554bc8409e48950a2eaa86fa7cc6721c0e0a8fd4
sabirbasha54 <sshabeer@maybank.com.sg>
2021-05-24 23:34:39 +0800

    Added Sintel Assignment

---
+++ ios/Pods/Target Support Files/Pods-sintelApp-sintelAppTests/Pods-sintelApp-sintelAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sintelApp_sintelAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_sintelApp_sintelAppTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-sintelApp/Pods-sintelApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sintelApp : NSObject
+@end
+@implementation PodsDummy_Pods_sintelApp
+@end
